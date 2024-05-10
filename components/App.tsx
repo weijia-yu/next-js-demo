c1c4b0de37a6558b359336ef6fb1f9dd70c30cce
Weijia <ywj7931@gmail.com>
2024-05-10 09:23:13 -0700

    first commit

---
+++ components/App.tsx
@@ -0,0 +1,104 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { RetellWebClient } from "retell-client-js-sdk";
+// import "./App.css";
+
+const agentId = "Your_agent_id";
+
+interface RegisterCallResponse {
+  call_id?: string;
+  sample_rate: number;
+}
+
+const webClient = new RetellWebClient();
+
+const App: React.FC = () => {
+  const [isCalling, setIsCalling] = useState(false);
+
+  // Initialize the SDK
+  useEffect(() => {
+    // Setup event listeners
+    webClient.on("conversationStarted", () => {
+      console.log("conversationStarted");
+    });
+
+    webClient.on("audio", (audio: Uint8Array) => {
+      console.log("There is audio");
+    });
+
+    webClient.on("conversationEnded", ({ code, reason }) => {
+      console.log("Closed with code:", code, ", reason:", reason);
+      setIsCalling(false); // Update button to "Start" when conversation ends
+    });
+
+    webClient.on("error", (error) => {
+      console.error("An error occurred:", error);
+      setIsCalling(false); // Update button to "Start" in case of error
+    });
+
+    webClient.on("update", (update) => {
+      // Print live transcript as needed
+      console.log("update", update);
+    });
+  }, []);
+
+  const toggleConversation = async () => {
+    if (isCalling) {
+      webClient.stopConversation();
+    } else {
+      const registerCallResponse = await registerCall(agentId);
+      console.log(registerCallResponse);
+      if (registerCallResponse.call_id) {
+        webClient
+          .startConversation({
+            callId: registerCallResponse.call_id,
+            sampleRate: registerCallResponse.sample_rate,
+            enableUpdate: true,
+          })
+          .catch(console.error);
+        setIsCalling(true); // Update button to "Stop" when conversation starts
+      }
+    }
+  };
+
+  async function registerCall(agentId: string): Promise<RegisterCallResponse> {
+    try {
+      // Replace with your server url
+      const response = await fetch(
+        "http://localhost:8080/register-call-on-your-server",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            agent_id: agentId,
+          }),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      }
+
+      const data: RegisterCallResponse = await response.json();
+      return data;
+    } catch (err) {
+      console.log(err);
+      throw new Error(err as string);
+    }
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button onClick={toggleConversation}>
+          {isCalling ? "Stop" : "Start"}
+        </button>
+      </header>
+    </div>
+  );
+};
+
+export default App;
